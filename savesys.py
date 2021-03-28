c8a9e57d47d1c4644e38fd0bde8f5daaa7508079
draumaz <77967471+draumaz@users.noreply.github.com>
2021-03-28 13:46:28 -0700

    QoL fixes, redundant code removed

--- savesys.py
+++ savesys.py
@@ -1,11 +1,11 @@
 def saveWriter(line_ext, state_ext): #line_ext = line number from 0, state_ext = variable on line
+    state_write = str(state_ext) + '\n' #converts state_ext to string and affixes '\n' to write without splash damage
     file = open('data.txt', 'r') #opens the file for reading
     line = file.readlines() #collects line information
-    line[line_ext] = state_ext #inputs state_ext on line_ext
+    line[line_ext] = state_write #inputs state variable on supplied line
     file = open('data.txt', 'w') #opens file for writing
     file.writelines(line) #writes to file
     file.close() #closes file
-    return #returns to previous function
 
 def saveReader():
     file = open('data.txt', 'r') #opens file for reading
@@ -16,9 +16,8 @@ def saveReader():
 
 def saveWriteReq(): #template, use this function in other scripts and call back
     line_ext = 3 #sets line_ext (ex. line[3])
-    state_ext = '7\n' #sets state_ext (ex. 7) NOTE must be in string form, affixed with \n
+    state_ext = 7 #sets state_ext (ex. 7)
     saveWriter(line_ext, state_ext) #sends variables to script for processing
-    return #returns to previous function
 
 #USAGE:
 #saveWriteReq() start write process
