a41cd637c5f319b34fa25ecdbf22c1413f63e6b8
draumaz <77967471+draumaz@users.noreply.github.com>
2021-05-12 23:59:31 -0700

    Update example.py

--- example.py
+++ example.py
@@ -6,22 +6,21 @@
 
 import time
 import sys
-import os
 import savesys
 
-savesys.saveGeneration() #Create save
+savesys.saveGeneration() # Create save
 
-variables = savesys.saveReader() #Call reader
-var2 = variables[1] #assign var2 to second variable from array
+variables = savesys.saveReader() # Call reader
+var2 = variables[1] # Assign var2 to second variable from array
 
 print("Your second variable is", var2, end="") 
 print(".")
 
 time.sleep(1)
 
-line_ext = 1 #assign line 1 for writing
-state_ext = 2 #change state to 2
-savesys.saveWriter(line_ext, state_ext) #Call writer and pass variables
+line_ext = 1 # Assign line to 1
+state_ext = 2 # Assign state to 2
+savesys.saveWriter(line_ext, state_ext) # Call writer and pass variables
 
 variables = savesys.saveReader()
 var2 = variables[1]
