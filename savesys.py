6da6a04f53d4018161c873912ffcd0252e492e8e
draumaz <77967471+draumaz@users.noreply.github.com>
2021-05-22 17:30:17 -0700

    Update savesys.py

--- savesys.py
+++ savesys.py
@@ -2,20 +2,20 @@
 # Written by draumaz in 2021
 # BSD 3-Clause
 
-def saveWriter(line_ext, state_ext): #line_ext = line number from 0, state_ext = variable on line
-    state_write = str(state_ext) + '\n' #converts state_ext to string and affixes '\n' to write without splash damage
-    f = open('data.txt', 'r') #opens the file for reading
-    line = f.readlines() #collects line information
-    line[line_ext] = state_write #inputs state variable on supplied line
-    f = open('data.txt', 'w') #opens file for writing
-    f.writelines(line) #writes to file
-    f.close() #closes file
+def saveWriter(line_ext, state_ext): # line_ext = line number from 0, state_ext = variable on line
+    state_write = str(state_ext) + '\n' # Converts state_ext to string and affixes '\n' to write without splash damage
+    f = open('data.txt', 'r') # Opens the file for reading
+    line = f.readlines() # Collects line information
+    line[line_ext] = state_write # Inputs state variable on supplied line
+    f = open('data.txt', 'w') # Opens file for writing
+    f.writelines(line) # Writes to file
+    f.close() # Closes file
 
 def saveReader():
-    file = open('data.txt', 'r') #opens file for reading
-    line = file.readlines() #collects line information
-    a = int(line[0]) #assigns lines to variables, this should
-    b = int(line[1]) #be infinitely configurable.
+    file = open('data.txt', 'r') # Opens file for reading
+    line = file.readlines() # Collects line information
+    a = int(line[0]) # Assigns lines to variables, this should
+    b = int(line[1]) # be infinitely configurable.
     c = int(line[2])
     d = int(line[3])
     e = int(line[4])
@@ -23,10 +23,10 @@ def saveReader():
     #g = int(line[6])
     #h = int(line[7])
     #and so on...
-    file.close() #closes file
-    return [a, b, c, d, e] #returns variables in an array, add to array if you have added more variables e.g. [a, b, c, d, e, f, g, h, coolvariable, epicvariable]
+    file.close() # Closes file
+    return [a, b, c, d, e] # Returns variables in an array, add to array if you have added more variables e.g. [a, b, c, d, e, f, g, h, coolvariable, epicvariable]
 
-def saveGeneration(): #Save generator
-    f = open('data.txt', 'w+') #creates new file
-    f.write('0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0') #writes 15 zeroes (configurable, must match amount of variables in saveReader)
-    f.close() #closes the file
+def saveGeneration(): # Save generator
+    f = open('data.txt', 'w+') # Creates new file
+    f.write('0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0') # Writes 15 zeroes (configurable, must match amount of variables in saveReader)
+    f.close() # Closes the file
