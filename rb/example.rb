596d1c2dc529c0920744af88754d705de15b5c52
draumaz <77967471+draumaz@users.noreply.github.com>
2021-05-27 13:58:41 -0700

    Create example.rb

---
+++ rb/example.rb
@@ -0,0 +1,15 @@
+require './savesys'
+
+save_gen() # Generate save
+
+var2 = save_read[1] # Assign var2 to second item in array
+
+puts var2
+
+line_ext = 1 # Assign line to '1' (line 2)
+state_ext = 2 # Assign state to '2'
+save_write(line_ext, state_ext) # Write
+
+var2 = save_read[1] # Assign again
+
+puts var2
