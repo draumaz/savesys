da2601938bce6a4e5d4cbcd188abf073f3732c50
draumaz <77967471+draumaz@users.noreply.github.com>
2021-06-07 12:18:57 -0700

    Add files via upload

--- cpp/savesys.cpp
+++ cpp/savesys.cpp
@@ -6,38 +6,19 @@
 #include <fstream>
 
 void save_writer(int line, int state){ // line - line number from 0; state - variable to insert
-	int a,b,c,d,e; // ADJUSTABLE: Generate variables
+	static int v[5]; // ADJUSTABLE: Create array
 	std::ifstream write_in("data.txt"); // Open file for read
-	write_in >> a >> b >> c >> d >> e; // Assign lines to variables
-	if ( line == 0 ){ // ADJUSTABLE: Pointers
-		a = state;
-	}
-	if ( line == 1 ){
-		b = state;
-	}
-	if ( line == 2 ){
-		c = state;
-	}
-	if ( line == 3 ){
-		d = state;
-	}
-	if ( line == 4 ){
-		e = state;
-	}
+	write_in >> v[0] >> v[1] >> v[2] >> v[3] >> v[4]; // Assign lines to array
+	v[line] = state; // Director
 	std::ofstream write_out("data.txt"); // Open file for write
-	write_out << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n"; // ADJUSTABLE: Write to file
+	write_out << v[0] << "\n" << v[1] << "\n" << v[2] << "\n" << v[3] << "\n" << v[4] << "\n"; // ADJUSTABLE: Write to file
 }
 
 int * save_reader(){
 	int a,b,c,d,e; // ADJUSTABLE: Generate variables
 	static int read_array[4]; // ADJUSTABLE: Create array
 	std::ifstream read_in("data.txt"); // Open file for reading
-	read_in >> a >> b >> c >> d >> e; // ADJUSTABLE: Assign to variables
-	read_array[0] = a; // Assign array spaces as variables
-	read_array[1] = b;
-	read_array[2] = c;
-	read_array[3] = d;
-	read_array[4] = e;
+	read_in >> read_array[0] >> read_array[1] >> read_array[2] >> read_array[3] >> read_array[4]; // ADJUSTABLE: Assign to variables
 	return read_array; // Return array
 }
 
