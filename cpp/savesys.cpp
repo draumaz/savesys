fe2dfe21efddb36ba2d8f527382625b4f6ac4f56
draumaz <77967471+draumaz@users.noreply.github.com>
2021-06-09 18:48:59 -0700

    Update savesys.cpp

--- cpp/savesys.cpp
+++ cpp/savesys.cpp
@@ -6,16 +6,15 @@
 #include <fstream>
 
 void save_writer(int line, int state){ // line - line number from 0; state - variable to insert
-	static int v[5]; // ADJUSTABLE: Create array
+	static int write_array[5]; // ADJUSTABLE: Create array
 	std::ifstream write_in("data.txt"); // Open file for read
-	write_in >> v[0] >> v[1] >> v[2] >> v[3] >> v[4]; // Assign lines to array
-	v[line] = state; // Director
+	write_in >> write_array[0] >> write_array[1] >> write_array[2] >> write_array[3] >> write_array[4]; // Assign lines to array
+	write_array[line] = state; // Director
 	std::ofstream write_out("data.txt"); // Open file for write
-	write_out << v[0] << "\n" << v[1] << "\n" << v[2] << "\n" << v[3] << "\n" << v[4] << "\n"; // ADJUSTABLE: Write to file
+	write_out << write_array[0] << "\n" << write_array[1] << "\n" << write_array[2] << "\n" << write_array[3] << "\n" << write_array[4] << "\n"; // ADJUSTABLE: Write to file
 }
 
 int * save_reader(){
-	int a,b,c,d,e; // ADJUSTABLE: Generate variables
 	static int read_array[4]; // ADJUSTABLE: Create array
 	std::ifstream read_in("data.txt"); // Open file for reading
 	read_in >> read_array[0] >> read_array[1] >> read_array[2] >> read_array[3] >> read_array[4]; // ADJUSTABLE: Assign to variables
