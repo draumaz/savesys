f5872f2323fb3b1096da33fa1084400b6bedb3b4
draumaz <77967471+draumaz@users.noreply.github.com>
2021-07-02 00:21:24 -0700

    Update savesys.cs

--- cs/savesys.cs
+++ cs/savesys.cs
@@ -1,4 +1,6 @@
-// Non-functional, still in development //
+// Savesys, an easily-implementable save manage
+// Written by draumaz in 2021
+// BSD 3-Clause
 
 using System;
 using System.IO;
@@ -6,33 +8,67 @@ using System.Linq;
 
 namespace Savesys
 {
+    class Path_Settings
+    {
+        public static string Paths(int m)
+        {
+            string save_path = "data.txt"; // Customize here!
+            string save_len = "0\n0\n0\n0\n0";
+            if (m == 1)
+            {
+                return save_path;
+            }
+            if (m == 2)
+            {
+                return save_len;
+            }
+            return "Error";
+        }
+    }
     public class IO
     {
         public static int[] Save_Read()
         {
-            string[] x = File.ReadAllLines("data.txt");
-            int[] y = Array.ConvertAll(x, int.Parse);
-            return y;
+            int m = 1; // Path set
+            string[] x = File.ReadAllLines(Path_Settings.Paths(m)); // Write file lines to string array
+            int[] y = Array.ConvertAll(x, int.Parse); // Convert to int array
+            return y; // Return int array
         }
-        public static void Save_Write(int line, int state)
+        public static void Save_Write(int line, int state) // line - line number from 0; state - variable to insert
         {
-            int[] write_array = Save_Read();
-            write_array[line] = state;
+            int m = 1; // Path Set
+            int[] read_array = Save_Read(); // Fetch variables into array
+            read_array[line] = state; // Director
+            string[] write_array = read_array.Select(x => x.ToString()).ToArray(); // Convert to string array
+            File.WriteAllLines(Path_Settings.Paths(m), write_array); // Write to file
         }
         public static void Save_Gen()
         {
-            File.WriteAllText("data.txt", "0\n0\n0\n0\n0\n");
+            int m = 1; // Path Set
+            int mm = 2; // Path Set
+            string sl = Path_Settings.Paths(m);
+            string sp = Path_Settings.Paths(mm);
+            File.WriteAllText(sl, sp); // Write to file
         }
     }
 }
 
-namespace Test
+namespace Example
 {
-    public class Toest
+    public class Program
     {
         public static void Main()
         {
-            int[] i = Savesys.IO.Save_Read();
+            Savesys.IO.Save_Gen();
+            int[] before = Savesys.IO.Save_Read();
+            int be4 = before[1];
+            Console.WriteLine("Before conversion {0}", be4);
+            int line = 1; // Assign line to '1' (line 2)
+            int state = 5; // Assign state to '2'
+            Savesys.IO.Save_Write(line, state); // Write
+            int[] after = Savesys.IO.Save_Read();
+            int a4tr = after[1];
+            Console.WriteLine("After conversion {0}", a4tr);
         }
     }
 }
