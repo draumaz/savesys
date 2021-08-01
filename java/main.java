363aea403dbd13cc4067d836c943e84729f216b7
draumaz <77967471+draumaz@users.noreply.github.com>
2021-07-31 19:55:07 -0700

    Update main.java

--- java/main.java
+++ java/main.java
@@ -1,3 +1,9 @@
+// Savesys, an easily-implementable save manager
+// Written by draumaz in 2021
+// BSD 3-Clause
+
+// Example script
+
 class main {
     public static void main(String[] args) {
         savesys sv = new savesys();
