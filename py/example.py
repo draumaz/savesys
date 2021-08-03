c2228f81cf0cc469cdcaca54ee4c3591785ef01b
Sam <77967471+draumaz@users.noreply.github.com>
2021-08-02 18:51:58 -0700

    Add files via upload

--- py/example.py
+++ py/example.py
@@ -8,24 +8,16 @@ import time
 import sys
 import savesys
 
-savesys.saveGeneration() # Create save
+savesys.exists() # Generate/check file
 
-variables = savesys.saveReader() # Call reader
-var2 = variables[1] # Assign var2 to second variable from array
-
-print("Your second variable is", var2, end="") 
+print("Your second variable is", savesys.read()[1], end="")
 print(".")
 
 time.sleep(1)
 
-line_ext = 1 # Assign line to 1
-state_ext = 2 # Assign state to 2
-savesys.saveWriter(line_ext, state_ext) # Call writer and pass variables
+savesys.write(1, 7) # Write variable 7 to line 1
 
-variables = savesys.saveReader()
-var2 = variables[1]
-
-print("...but now it's", var2, end="")
+print("...but now it's", savesys.read()[1], end="")
 print('!')
 
 time.sleep(2)
--- py/savesys.py
+++ py/savesys.py
@@ -2,31 +2,24 @@
 # Written by draumaz in 2021
 # BSD 3-Clause
 
-def saveWriter(line_ext, state_ext): # line_ext = line number from 0, state_ext = variable on line
-    state_write = str(state_ext) + '\n' # Converts state_ext to string and affixes '\n' to write without splash damage
-    f = open('data.txt', 'r') # Opens the file for reading
-    line = f.readlines() # Collects line information
-    line[line_ext] = state_write # Inputs state variable on supplied line
-    f = open('data.txt', 'w') # Opens file for writing
-    f.writelines(line) # Writes to file
-    f.close() # Closes file
+import os
 
-def saveReader():
-    file = open('data.txt', 'r') # Opens file for reading
-    line = file.readlines() # Collects line information
-    a = int(line[0]) # Assigns lines to variables, this should
-    b = int(line[1]) # be infinitely configurable.
-    c = int(line[2])
-    d = int(line[3])
-    e = int(line[4])
-    #f = int(line[5])
-    #g = int(line[6])
-    #h = int(line[7])
-    #and so on...
-    file.close() # Closes file
-    return [a, b, c, d, e] # Returns variables in an array, add to array if you have added more variables e.g. [a, b, c, d, e, f, g, h, coolvariable, epicvariable]
+def config(pos):
+    if pos == 0:
+        return "default.txt" # This is what you want your save to be called...
+    elif pos == 1:
+        return 20 # ...and this is how many zeroes you want it to contain.
 
-def saveGeneration(): # Save generator
-    f = open('data.txt', 'w+') # Creates new file
-    f.write('0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0\n0') # Writes 15 zeroes (configurable, must match amount of variables in saveReader)
-    f.close() # Closes the file
+def exists():
+    if (os.path.exists()): # If the file already exists, don't overwrite it...
+        pass
+    else: # ...otherwise, create and write to the file!
+        open(config(0), "w+").write(config(1)*"0\n")
+
+def read():
+    return open(config(0), "r").readlines() # Return the entire file as an array of integers.
+
+def write(line, state):
+    i = read() # Retrieve the current states...
+    i[line] = str(state) + "\n" # Replace the requested line with requested state...
+    open("data.txt", "w+").writelines(i) # ...and write it to the file!
