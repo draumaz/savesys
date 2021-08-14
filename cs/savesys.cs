f3c54fa40de8431c3792cc8da7087620574a9958
Sam <77967471+draumaz@users.noreply.github.com>
2021-08-14 05:19:18 -0700

    Update savesys.cs

--- cs/savesys.cs
+++ cs/savesys.cs
@@ -52,23 +52,3 @@ namespace Savesys
         }
     }
 }
-
-namespace Example
-{
-    public class Program
-    {
-        public static void Main()
-        {
-            Savesys.IO.Save_Gen();
-            int[] before = Savesys.IO.Save_Read();
-            int be4 = before[1];
-            Console.WriteLine("Before conversion {0}", be4);
-            int line = 1; // Assign line to '1' (line 2)
-            int state = 5; // Assign state to '2'
-            Savesys.IO.Save_Write(line, state); // Write
-            int[] after = Savesys.IO.Save_Read();
-            int a4tr = after[1];
-            Console.WriteLine("After conversion {0}", a4tr);
-        }
-    }
-}
