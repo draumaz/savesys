090c88071c8d959f78adc05dfd6a91bd7f054437
Sam <77967471+draumaz@users.noreply.github.com>
2021-09-23 00:18:45 -0700

    Add files via upload

--- cpp/savesys.cpp
+++ cpp/savesys.cpp
@@ -5,23 +5,41 @@
 #include <iostream>
 #include <fstream>
 
-void save_writer(int line, int state){ // line - line number from 0; state - variable to insert
-	static int write_array[5]; // ADJUSTABLE: Create array
-	std::ifstream write_in("data.txt"); // Open file for read
-	write_in >> write_array[0] >> write_array[1] >> write_array[2] >> write_array[3] >> write_array[4]; // Assign lines to array
-	write_array[line] = state; // Director
-	std::ofstream write_out("data.txt"); // Open file for write
-	write_out << write_array[0] << "\n" << write_array[1] << "\n" << write_array[2] << "\n" << write_array[3] << "\n" << write_array[4] << "\n"; // ADJUSTABLE: Write to file
+const char* string_paths(int pnt) {
+	const char* path = NULL;
+	switch (pnt) {
+		case 0:
+			path = "data.txt"; // Adjustable file name
+			break;
+	}
+	return path;
 }
 
-int * save_reader(){
-	static int read_array[4]; // ADJUSTABLE: Create array
-	std::ifstream read_in("data.txt"); // Open file for reading
-	read_in >> read_array[0] >> read_array[1] >> read_array[2] >> read_array[3] >> read_array[4]; // ADJUSTABLE: Assign to variables
-	return read_array; // Return array
+int * save_reader() {
+	static int array[5]; // Adjust number
+	std::ifstream read_in(string_paths(0));
+	for (int i = 0; i < 5; i++) { // Adjust number
+		read_in >> array[i]; // Loop read lines into array
+	}
+	return array;
+}
+
+void save_writer(int line, int state) {
+	int * save_in = save_reader(); // Pull current values
+	std::ofstream save_out(string_paths(0));
+	for (int i = 0; i < 5; i++) { // Adjust number
+		if (i == line) {
+			save_out << state; // If loop is on supplied line, insert supplied state
+		} else {
+			save_out << save_in[i]; // Otherwise, reprint from save_in
+		}
+		save_out << "\n";
+	}
 }
 
 void save_generation(){
-	std::ofstream gen("data.txt"); // ADJUSTABLE: Create file
-	gen << "0\n0\n0\n0\n0\n"; // ADJUSTABLE: Write to file
+	std::ofstream gen(string_paths(0)) ;
+	for (int i = 0; i < 5; i++) { // Adjust number
+		gen << "0\n"; // Loop write five zeroes on new lines
+	}
 }
