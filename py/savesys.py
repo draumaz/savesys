75036ed94bc84af01b57f2edeedd1841905a6896
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-04 09:49:49 +0000

    Add files via upload

--- py/savesys.py
+++ py/savesys.py
@@ -4,24 +4,23 @@
 
 import os
 
-def config(ndl):
-    if ndl == 0:
-        return "default.txt" # This is what you want your save to be called...
-    elif ndl == 1:
-        return 20 # ...this is how many lines you want it to contain (it starts from 0!)...
-    elif ndl == 2:
-        return 0 # ...and this is what you want to fill each line with.
+## begin config ##
+save_name = "default.txt"
+save_size = 20
+save_contents = "0"
+## end config ##
+
 def exists():
-    if not (os.path.exists(config(0))): # If the file doesn't exist...
-        open(config(0), "w+").write(config(1)*(str(config(2))+"\n")) # ...create it, and fill it using config values.
+    if not (os.path.exists(save_name)): # If the file doesn't exist...
+        open(save_name, "w+").write(save_size*(save_contents+"\n")) # ...create it, and fill it using config values.
 
 def read():
-    return list(map(int, open(config(0), "r").readlines())) # Return the entire file as an array of integers.
+    return list(map(int, open(save_name, "r").readlines())) # Return the entire file as an array of integers.
 
 def write(line, state):
     arr = read() # Retrieve the current states...
-    f = open(config(0), "w") # ...open the file...
-    for i in range(0, (config(1))): # ...set loop ranges....
+    f = open(save_name, "w") # ...open the file...
+    for i in range(0, save_size): # ...set loop ranges....
         if i == line:
             f.write(str(state)+"\n") # ...if the loop matches line, write state...
             continue
