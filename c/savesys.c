ee4be1b26c5b10c5158c60a1f92d23d39d434d7a
Sam <77967471+draumaz@users.noreply.github.com>
2022-01-31 17:25:55 -0800

    Global length determiner

--- c/savesys.c
+++ c/savesys.c
@@ -5,25 +5,27 @@
 #include <unistd.h>
 
 // begin config
+#define save_length 12
 const char* save_name = "data.txt";
 // end config
 
 int * save_reader() {
-	static int array[12]; // adjust 12 to desired length
+	static int array[12];
+	int i = 0;
+	int x = 0;
 	FILE *read_in = fopen(save_name, "r");
-	int i = 0; int j = 0; int x = 0;
-	j = fscanf(read_in, "%d", &i);
+	fscanf(read_in, "%d", &i);
 	while (!feof (read_in)) {
 		array[x] = i; x += 1;
-		j = fscanf(read_in, "%d", &i);
-	} j += 1; return array;
+		fscanf(read_in, "%d", &i);
+	} return array;
 }
 
 void save_writer(int line, int state) {
 	if (state < 0) { state = 0; }
 	int * save_in = save_reader();
 	FILE *read_out = fopen(save_name, "w");
-	for (int i = 0; i < 12; i++) { // adjust 12 to desired length
+	for (int i = 0; i < save_length; i++) {
 		if (i == line) {
 			fprintf(read_out, "%d\n", state);
 		} else {
@@ -34,7 +36,7 @@ void save_writer(int line, int state) {
 
 void save_generate() {
 	FILE *generate = fopen(save_name, "w");
-	for (int i = 0; i < 12; i++) { // adjust 12 to desired length
+	for (int i = 0; i < save_length; i++) {
 		fprintf(generate, "%d\n", 0);
 	} fclose(generate);
 }
