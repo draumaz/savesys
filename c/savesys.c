ee25e1b64710c2426acefddbae27418b43c272f4
draumaz <77967471+draumaz@users.noreply.github.com>
2022-02-23 16:20:51 -0800

    Improvements on C port

--- c/savesys.c
+++ c/savesys.c
@@ -5,44 +5,46 @@
 #include <unistd.h>
 
 // begin config
-#define save_length 12
-const char* save_name = "data.txt";
+#define SAVE_LENGTH 12
+#define SAVE_NAME "data.txt"
 // end config
 
 int * save_reader() {
-	static int array[12];
-	int i = 0;
-	int x = 0;
-	FILE *read_in = fopen(save_name, "r");
-	fscanf(read_in, "%d", &i);
-	while (!feof (read_in)) {
-		array[x] = i; x += 1;
-		fscanf(read_in, "%d", &i);
-	} return array;
+	static int array[SAVE_LENGTH];
+	FILE *read_in = fopen(SAVE_NAME, "r");
+	for (int i = 0; i < SAVE_LENGTH; i++) {
+		fscanf(read_in, "%d", &array[i]);
+	}
+	fclose(read_in);
+	return array;
 }
 
 void save_writer(int line, int state) {
 	if (state < 0) { state = 0; }
 	int * save_in = save_reader();
-	FILE *read_out = fopen(save_name, "w");
-	for (int i = 0; i < save_length; i++) {
+	FILE *read_out = fopen(SAVE_NAME, "w");
+	for (int i = 0; i < SAVE_LENGTH; i++) {
 		if (i == line) {
 			fprintf(read_out, "%d\n", state);
 		} else {
 			fprintf(read_out, "%d\n", save_in[i]);
 		}
-	} fclose(read_out);
+	}
+	fclose(read_out);
 }
 
 void save_generate() {
-	FILE *generate = fopen(save_name, "w");
-	for (int i = 0; i < save_length; i++) {
+	FILE *generate = fopen(SAVE_NAME, "w");
+	for (int i = 0; i < SAVE_LENGTH; i++) {
 		fprintf(generate, "%d\n", 0);
-	} fclose(generate);
+	}
+	fclose(generate);
 }
 
 void save_exists() {
-	if (access(save_name,F_OK) == -1) {
+	FILE *f;
+	if ((f = fopen(SAVE_NAME, "r"))) {
+	} else {
 		save_generate();
 	}
 }
