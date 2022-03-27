28d1a277b3f736c83e1d63b9f70029c7c354d943
Sam <77967471+draumaz@users.noreply.github.com>
2022-03-27 13:51:17 -0700

    codebase complete

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,39 +1,10 @@
-use std::fs::File;
-use std::io::{prelude::*, Write, BufReader};
-use std::path::Path;
-
-// BEGIN CONFIG //
-
-static SAVE_NAME: &'static str = "data.txt";
-static SAVE_SIZE: i32          = 20;
-
-// END CONFIG //
-
-fn save_reader() -> Vec<i32> {
-    let file = File::open(SAVE_NAME).unwrap();
-    let v: Vec<i32> = BufReader::new(file)
-        .lines()
-        .flatten()
-        .flat_map(|line| line.parse::<i32>())
-        .collect();
-    return v;
-}
-
-fn save_generate() {
-    let mut file = File::create(SAVE_NAME).unwrap();
-    for i in 0..SAVE_SIZE {
-        file.write(b"0\n").unwrap();
-    }
-}
-
-fn save_exists() {
-    if Path::new(SAVE_NAME).exists() == false {
-        save_generate();
-    }
-}
+mod savesys;
 
 fn main() {
-    save_exists();
-    let i = save_reader();
-    println!("{}", i[2]);
+    savesys::exists();
+    let mut i = savesys::reader();
+    println!("- Line two: {}\n- calling save_writer(2, 7)", i[2]);
+    savesys::writer(2, 7);
+    i = savesys::reader();
+    println!("- Line two: {}", i[2]);
 }
