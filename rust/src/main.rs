fe3f0f75ab5108e84c54787fb97beb8e32c769a1
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-26 20:53:36 -0700

    work

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -10,14 +10,18 @@ static SAVE_SIZE: i32          = 20;
 // END CONFIG //
 
 fn save_generate() {
+    println!("Creating file: ./{}", SAVE_NAME);
     let mut file = File::create(SAVE_NAME).unwrap();
-    for _ in 0..SAVE_SIZE {
+    println!("Begin file write loop");
+    for i in 0..SAVE_SIZE {
+        println!("Writing to line {}", i);
         file.write(b"0\n").unwrap();
     }
 }
 
 fn save_exists() {
     if Path::new(SAVE_NAME).exists() == false {
+        println!("File does not exist, calling generator");
         save_generate();
     } else {
         println!("File already exists");
