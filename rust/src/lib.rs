5dd5cebb6a55272d1b1242cb0b17f3c7f28c004e
Sam <77967471+draumaz@users.noreply.github.com>
2022-03-27 17:05:41 -0700

    in-file config removed

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,15 +2,8 @@ use std::fs::{File, OpenOptions};
 use std::io::{prelude::*, Write, BufReader};
 use std::path::Path;
 
-// BEGIN CONFIG //
-
-static SAVE_NAME: &'static str = "data.txt";
-static SAVE_SIZE: i32          = 20;
-
-// END CONFIG //
-
-pub fn reader() -> Vec<i32> {
-    let file = File::open(SAVE_NAME).expect("Couldn't open file for reading");
+pub fn reader(save_name: &'static str) -> Vec<i32> {
+    let file = File::open(save_name).expect("Couldn't open file for reading");
     return BufReader::new(file)
         .lines()
         .flatten()
@@ -18,24 +11,24 @@ pub fn reader() -> Vec<i32> {
         .collect();
 }
 
-pub fn writer(line: usize, state: i32) {
-    let mut read = reader();
-    let mut file = OpenOptions::new().write(true).open(SAVE_NAME).expect("Couldn't open file for writing");
+pub fn writer(save_name: &'static str, line: usize, state: i32) {
+    let mut read = reader(save_name);
+    let mut file = OpenOptions::new().write(true).open(save_name).expect("Couldn't open file for writing");
     read[line] = state;
     for i in read {
         write!(file, "{}\n", i).expect("Couldn't write to file.");
     }
 }
 
-pub fn generate() {
-    let mut file = File::create(SAVE_NAME).expect("Couldn't create file");
-    for _ in 0..SAVE_SIZE {
+pub fn generate(save_name: &'static str, save_size: i32) {
+    let mut file = File::create(save_name).expect("Couldn't create file");
+    for _ in 0..save_size {
         file.write(b"0\n").expect("Couldn't write to file");
     }
 }
 
-pub fn exists() {
-    if Path::new(SAVE_NAME).exists() == false {
-        generate();
+pub fn exists(save_name: &'static str, save_size: i32) {
+    if Path::new(save_name).exists() == false {
+        generate(save_name, save_size);
     }
 }
