3b49952d7f1efe846d1492a34d7f3dbbb59248c4
draumaz <77967471+draumaz@users.noreply.github.com>
2022-03-27 19:51:05 -0700

    version bump for crates

--- rust/target/package/savesys-3.5.4/target/debug/deps/savesys-18e2a8921fa0ea3b.d
+++
@@ -1,7 +0,0 @@
-/home/samuel/Programming/projects/savesys/rust/target/package/savesys-3.5.4/target/debug/deps/savesys-18e2a8921fa0ea3b.rmeta: src/lib.rs
-
-/home/samuel/Programming/projects/savesys/rust/target/package/savesys-3.5.4/target/debug/deps/libsavesys-18e2a8921fa0ea3b.rlib: src/lib.rs
-
-/home/samuel/Programming/projects/savesys/rust/target/package/savesys-3.5.4/target/debug/deps/savesys-18e2a8921fa0ea3b.d: src/lib.rs
-
-src/lib.rs:
--- rust/target/package/savesys-3.5.4/target/debug/libsavesys.d
+++
@@ -1,1 +0,0 @@
-/home/samuel/Programming/projects/savesys/rust/target/package/savesys-3.5.4/target/debug/libsavesys.rlib: /home/samuel/Programming/projects/savesys/rust/target/package/savesys-3.5.4/src/lib.rs
