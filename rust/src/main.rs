165e3503390698bf2627134510e36ec5ee4e84c8
Sam <77967471+draumaz@users.noreply.github.com>
2022-04-10 23:43:26 -0700

    Update main.rs

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,9 +1,10 @@
-use savesys;
+extern crate savesys;
+use savesys::{reader, writer, exists};
 
 fn main() {
     savesys::exists("data.txt", 20); // Generate/check file (20 is the length of the file)
 
-    let mut i = savesys::reader("data.txt"); // Store reader vector in i
+    let mut i: <Vec:i32> = savesys::reader("data.txt"); // Store reader vector in i
     println!("- Line two: {}\n- calling save_writer(2, 7)", i[2]);
 
     savesys::writer("data.txt", 2, 7); // Write variable 7 to line 2
