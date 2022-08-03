9f4b3828a50244f76d86660f1f416f8bc6c645ed
draumaz <draumaz@goatopossum.com>
2022-08-03 05:08:54 +0000

    Update 'rust/src/main.rs'

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,9 +2,8 @@ extern crate savesys;
 use savesys::*;
 
 fn main() {
-    if exists("data.txt") == false { generate("data.txt", 20) };
+    if ! exists("data.txt") { generate("data.txt", 20) };
 
     let save_vec: Vec<i32> = savesys::reader("data.txt");
-    
-    if save_vec[5] != 7 { writer("data.txt", 5, 7) }
+    if ! save_vec[5] == 7 { writer("data.txt", 5, 7) }
 }
